7b3c5b5e08f27a61e0e70cd6853ab1ca6175ae36
sharanNovelty <ram@noveltytechnology.com>
2024-04-11 19:45:30 +0545

    Add condition

--- src/components/Input/ToogleInput.js
+++ src/components/Input/ToogleInput.js
@@ -13,8 +13,10 @@ function ToogleInput({labelTitle, labelStyle, type, containerStyle, defaultValue
     return(
         <div className={`form-control w-full ${containerStyle}`}>
             <label className="label cursor-pointer">
-                <span className={"label-text text-base-content " + labelStyle}>{labelTitle}</span>
-                <input type="checkbox" className="toggle" checked={value}  onChange={(e) => updateToogleValue()}/>
+                {labelTitle && 
+                    <span className={"label-text text-base-content " + labelStyle}>{labelTitle}</span>
+                }
+                <input type="checkbox" className="toggle" checked={value} placeholder={placeholder} onChange={(e) => updateToogleValue()}/>
             </label>
         </div>
     )
